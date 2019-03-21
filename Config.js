476d89fefaf3e5f5e748f60457dcec10913a9519
Fedor B Gorsky <fedor@support-pc.org>
2019-03-21 15:11:53 +0300

    Fix deprecation in firebase

--- Config.js
+++ Config.js
@@ -33,30 +33,9 @@ const config = {
     //Время кеширования на небольшой длительны времени в минутах
     highCache: 100,
 
-    firebase: {
-        appId: "1:552523161732:android:90582f78acdd4ec1",
-        apiKey: "AIzaSyDdt9Dc6lcyrLHphwL8WTflG3AKsu-HgkE",
-        // authDomain: "<YOUR-AUTH-DOMAIN>",
-        databaseURL: "https://banket-b.firebaseio.com/",
-        // messagingSenderId: '123',
-        projectId: 'banket-b',
-        storageBucket: 'restorate-battle.appspot.com'
-    },
-
-    // firebase: {
-    //     appId: "1:911932262915:android:1fb0623114da33be",
-    //     apiKey: "AIzaSyDDGo2kRmSJ_oORXgf-I-cNgBM2JMyX93Q",
-    //     // authDomain: "<YOUR-AUTH-DOMAIN>",
-    //     databaseURL: "https://restorate-battle.firebaseio.com",
-    //     // messagingSenderId: '123',
-    //     projectId: 'restorate-battle',
-    //     storageBucket: 'restorate-battle.appspot.com'
-    // },
-
-
     sentryDSN: 'https://853a3e3476854893b66e1c1652f3ad90@sentry.io/1328643'
 };
-export const firebase = Firebase.initializeApp(config.firebase);
+export const firebase = Firebase.app();
 export const db = firebase.database();
 
 export default config;
